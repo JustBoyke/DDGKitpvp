3be9e6f52a1afbbd54fd52dfd4f718fe7303bb0e
JustBoyke <b.vanvugt@fire-enterprise.nl>
2019-09-30 22:04:24 +0200

    Progress: 8%

---
+++ kitpvp/src/main/java/me/boykev/kitpvp/EventManager.java
@@ -0,0 +1,40 @@
+package me.boykev.kitpvp;
+
+import java.util.HashMap;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class EventManager implements Listener{
+	
+	public EventManager(Main main) {
+		this.instance = main;
+	}
+	private Main instance;
+	private ConfigManager cm;
+	
+	public void onSignClick(PlayerInteractEvent e) {
+		HashMap<Player, Integer> mapid = instance.mapid;
+		cm = new ConfigManager(instance);
+		
+		if(e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if(e.getClickedBlock().getType() == Material.SIGN || e.getClickedBlock().getType() == Material.WALL_SIGN) {
+				Sign sign = (Sign) e.getClickedBlock().getState();
+				String signpref = ChatColor.translateAlternateColorCodes('&', cm.getConfig().getString("Sign-Prefix"));
+				if(sign.getLine(0).equalsIgnoreCase(signpref)) {
+					
+				}
+				return;
+			}
+			return;
+		}
+		return;
+		
+	}
+	
+}
--- kitpvp/src/main/java/me/boykev/kitpvp/Main.java
+++ kitpvp/src/main/java/me/boykev/kitpvp/Main.java
@@ -1,12 +1,21 @@
 package me.boykev.kitpvp;
 
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin{
 	
+	public HashMap<Player, Integer> mapid = new HashMap<Player, Integer>();
+	public ConfigManager cm;
+	
 	public void onEnable() {
+		cm = new ConfigManager(this);
+		cm.LoadDefaults();
+		cm.save();
 		System.out.println(ChatColor.GREEN + "KitPVP is nu opgestart :)");
 	}
 	
@@ -17,8 +26,9 @@ public class Main extends JavaPlugin{
 	
 	//todo:
 	// In-Game lobby systeem voor deathspawn -V
-		// Instellen door elke game een ID te geven en te koppelen aan de spawn informatie
-	// instelbare spawns
+		// Instellen door elke game een ID te geven en te koppelen aan de spawn informatie bij join event in hashmap zetten
+	// instelbare spawns -V
+		// Via SIGNLINK (Sign met naam corrospondeerd aan de locatie in config, instelbaar met commando)
 	// bodje om te joinen
 	// menu voor instelbare kits
 	// opslaan kills / deaths in database
